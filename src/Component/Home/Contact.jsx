945de1acce65178378212b043ae2784d8f63592e
adhilsalahh <adhilsalahhk@gmail.com>
2025-01-31 16:02:58 +0530

    add gmail

--- src/Component/Home/Contact.jsx
+++ src/Component/Home/Contact.jsx
@@ -78,7 +78,7 @@ const Contact = () => {
                     d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                   />
                 </svg>
-                <p className="text-gray-600">info@yourcompany.com</p>
+                <p className="text-gray-600">vaorutrippadikam@gmail.com</p>
               </div>
               <div className="flex items-center gap-3">
                 <svg
